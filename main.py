69b455bd4ccb416914e07784dde4c4f555989552
pedro <pedroph99@outlook.com>
2023-03-27 21:43:56 -0300

    'bugs_resolvidos'

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import curses
 import math
 import time
 import curses.textpad as ct
-#imports the class snake from the file snake_body, wich is in the same folder
-from snake_body import snake
 
-snake_1 = snake()
+
+
+
 
 def run(stdscr):
-    stdscr.clear()
 
+    stdscr.clear()
     w_values = stdscr.getmaxyx()
     screen = curses.newwin(w_values[0]-3,w_values[1],0,0)
-    
     screen.keypad(True)
     screen.box('|','-')
     pad = curses.newpad(2,2)
@@ -26,7 +25,7 @@ def run(stdscr):
     y = 0
     array_saved = []
     pad.refresh(0,0,math.floor((w_values[0]-3)/2)+x,math.floor(w_values[1]/2)+y,math.floor((w_values[0]-3)/2)+x,math.floor(w_values[1]/2)+y)
-    pad_2.refresh(0,0,11,1,math.floor((w_values[0]-1)),math.floor(w_values[1]-1))
+    pad_2.refresh(0,0,math.floor((w_values[0]-3)),1,math.floor((w_values[0]-1)),math.floor(w_values[1]-1))
     turtle_commands = []
     string_comand = ''
     while True:
@@ -65,16 +64,16 @@ def run(stdscr):
                 for x_turtle in range(len(turtle_commands)):
                     pad_2.addstr(1,18 + x_turtle, turtle_commands[x_turtle])
                 pad_2.box('|', '-')
-                pad_2.refresh(0,0,11,1,math.floor((w_values[0]-1)),math.floor(w_values[1]-1))
+                pad_2.refresh(0,0,math.floor((w_values[0]-3)),1,math.floor((w_values[0]-1)),math.floor(w_values[1]-1))
 
             elif (ord(key)>=65 and ord(key)<=90) or (ord(key)>=48 and ord(key)<=57):
                 pad_2.addstr(1, 18 + len(turtle_commands), key)
                 turtle_commands.append(key)
-                pad_2.refresh(0,0,11,1,math.floor((w_values[0]-1)),math.floor(w_values[1]-1))
+                pad_2.refresh(0,0,math.floor((w_values[0]-3)),1,math.floor((w_values[0]-1)),math.floor(w_values[1]-1))
             elif ord(key) == 32:
                 pad_2.addstr(1, 18 + len(turtle_commands), key)
                 turtle_commands.append(' ')
-                pad_2.refresh(0,0,11,1,math.floor((w_values[0]-1)),math.floor(w_values[1]-1))
+                pad_2.refresh(0,0,math.floor((w_values[0]-3)),1,math.floor((w_values[0]-1)),math.floor(w_values[1]-1))
 
         except:
             pass
@@ -90,10 +89,13 @@ def run(stdscr):
         screen.clear()
         screen.box('|','-')
         screen.refresh()
-        for x2 in array_saved:
-            screen.addstr(x2[0], x2[1], '-')
         pcx = math.floor((w_values[0]-3)/2)+x
         pcy = math.floor(w_values[1]/2)+y
+        for x2 in array_saved:
+            if x2[0] != pcx or x2[1] != pcy:
+                screen.addstr(x2[0], x2[1], '-')
+            else:
+                screen.addstr(x2[0], x2[1], '@')
         array_saved.append([pcx,pcy])
         
         pad.refresh(0,0,math.floor((w_values[0]-3)/2)+x,math.floor((w_values[1])/2)+y,math.floor((w_values[0] -3)/2)+x,math.floor(w_values[1]/2)+y)
@@ -102,9 +104,5 @@ def run(stdscr):
 
         
         
-
-    
-    
-
 
 curses.wrapper(run)
