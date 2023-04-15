2cd012c3b53488a6845dde7a2a26de6a33357b9b
pedro <pedroph99@outlook.com>
2023-04-15 17:28:52 -0300

    'jogo_base_finalizado'

---
+++ functions.py
@@ -0,0 +1,67 @@
+
+import math
+import time
+# Funções auxiliares ao código main.
+
+def refresh_tela(pad,w_values,x,y):
+    pad.refresh(0,0,math.floor((w_values[0]-3)/2)+x,math.floor((w_values[1])/2)+y,math.floor((w_values[0] -3)/2)+x,math.floor(w_values[1]/2)+y)
+
+
+def refresh_pad2(pad_2,w_values):
+    pad_2.refresh(0,0,math.floor((w_values[0]-3)),0,math.floor((w_values[0]-1)),math.floor(w_values[1]-1))
+
+
+def desenha_tela(tela, x,y, simbolo):
+    tela.addstr(x,y,simbolo)
+
+
+def refresh_pad(pad, w_values, x,y):
+    pad.refresh(0,0,math.floor((w_values[0]-3)/2)+x,math.floor((w_values[1])/2)+y,math.floor((w_values[0] -3)/2)+x,math.floor(w_values[1]/2)+y)
+
+
+def refresh_main_screen(screen):
+    screen.clear()
+    screen.box('|','-')
+    screen.refresh()
+
+def verify_x(w_values, x):
+    if math.floor(w_values[0]/2)+x<=2:
+        return math.floor(w_values[0]/2)-3
+    elif math.floor(w_values[0]/2)+x == w_values[0]-2:
+        return -(math.floor(w_values[0]/2)-3)
+    else:
+        return x
+    
+def verify_y(w_values, y):
+    print(math.floor(w_values[1]/2)+y)
+    if math.floor(w_values[1]/2)+y<=1:
+        print('FOI PARA BAIXO DE 0')
+        print(w_values[1])
+        return math.floor(w_values[1]/2)-2
+    elif math.floor(w_values[1]/2)+y == w_values[1]-2:
+        print('chegou ao ponto')
+        print(-(math.floor(w_values[1]/2)-2))
+        return -(math.floor(w_values[1]/2)-2)
+    else:
+        return y
+
+def draw_directions(screen, w_values, array_saved, pad, x, y):
+    screen.clear()
+    screen.box('|','-')
+                            
+    pcx = math.floor((w_values[0]-3)/2)+x
+    pcy = math.floor(w_values[1]/2)+y
+    print(array_saved)
+    for x2 in array_saved:
+        if x2[0] != pcx or x2[1] != pcy:
+            desenha_tela(screen, x2[0], x2[1], '*')
+        else:
+            desenha_tela(screen, x2[0], x2[1], '@')
+                            
+    if [pcx, pcy] not in array_saved:
+        array_saved.append([pcx,pcy ])
+                            
+    screen.refresh()
+    refresh_pad(pad, w_values, x,y)
+                            
+    time.sleep(0.25)
--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 import curses
 import math
 import time
-import curses.textpad as ct
 import re
 
-
+from functions import *
 
 
 
@@ -20,13 +19,13 @@ def run(stdscr):
     pad_2 = curses.newpad(w_values[0] -(w_values[0]-3), w_values[1]-1)
     pad_2.box('|', '-')
     screen.refresh()
-    pad.addstr(0,0, "@")
-    pad_2.addstr(1,1, "Comando turtle: ")
+    desenha_tela(pad, 0,0, "@")
+    desenha_tela(pad_2,1,1,"Comando turtle: ")
     x = 0
     y = 0
     array_saved = []
-    pad.refresh(0,0,math.floor((w_values[0]-3)/2)+x,math.floor(w_values[1]/2)+y,math.floor((w_values[0]-3)/2)+x,math.floor(w_values[1]/2)+y)
-    pad_2.refresh(0,0,math.floor((w_values[0]-3)),1,math.floor((w_values[0]-1)),math.floor(w_values[1]-1))
+    refresh_pad(pad, w_values, x, y)
+    refresh_pad2(pad_2, w_values)
     turtle_commands = []
     string_comand = ''
     while True:
@@ -37,101 +36,128 @@ def run(stdscr):
             if ord(key) == 8:
                 turtle_commands.pop()
                 pad_2.clear()
-                pad_2.addstr(1,1, "Comando turtle: ")
+                desenha_tela(pad_2, 1,1, 'Comando turtle')
                 for x_turtle in range(len(turtle_commands)):
-                    pad_2.addstr(1,18 + x_turtle, turtle_commands[x_turtle])
+                    desenha_tela(pad_2, 1,18+x_turtle,  turtle_commands[x_turtle])
                 pad_2.box('|', '-')
-                pad_2.refresh(0,0,math.floor((w_values[0]-3)),1,math.floor((w_values[0]-1)),math.floor(w_values[1]-1))
+                refresh_pad2(pad_2, w_values)
             
             elif ord(key) == 10:
                 string_comand = ''
                 for x_string in turtle_commands:
                     string_comand+=x_string
-                if re.fullmatch((r"(NE|NO|N|SO|SL|S|O|E)\s*[0-9]*"), string_comand):
+                if re.fullmatch((r"(NE|NO|N|SO|SL|S|O|E|L)\s*[0-9]*"), string_comand):
                     print(string_comand.split(' ')[1])
-
-                    if string_comand.split(' ')[0] == 'E':
-                        for x_times in range(string_comand.split[' '][1]):
-                            print(x_times)
-                            y+=1
-                            pad.refresh(0,0,math.floor((w_values[0]-3)/2)+x,math.floor((w_values[1])/2)+y,math.floor((w_values[0] -3)/2)+x,math.floor(w_values[1]/2)+y)
-                    elif string_comand.split(' ')[0] == 'NE':
-                        print('okk')
+                    
+                    if string_comand.split(' ')[0] == 'NE':
                         print(string_comand.split(' ')[1])
                         for x_times in range(int(string_comand.split(' ')[1])):
-
                             y+=1
                             x-=1
-                            screen.clear()
-                            screen.box('|','-')
+                            x = verify_x(w_values, x)
+                            y = verify_y(w_values, y)
+                            draw_directions(screen, w_values, array_saved, pad, x, y)
+                    elif string_comand.split(' ')[0] == 'NO':
+                        print(string_comand.split(' ')[1])
+                        for x_times in range(int(string_comand.split(' ')[1])):
+                            y-=1
+                            x+=1
+                            x = verify_x(w_values, x)
+                            y = verify_y(w_values, y)
+                            draw_directions(screen, w_values, array_saved, pad, x, y)
+
+                    elif string_comand.split(' ')[0] == 'N':
+                        print(string_comand.split(' ')[1])
+                        for x_times in range(int(string_comand.split(' ')[1])):
+                            x-=1
+                            x = verify_x(w_values, x)
+
+                            draw_directions(screen, w_values, array_saved, pad, x, y)
+                    elif string_comand.split(' ')[0] == 'S':
+                        for x_times in range(int(string_comand.split(' ')[1])):
+                            x+=1
+                            x = verify_x(w_values, x)
+                            draw_directions(screen, w_values, array_saved, pad, x, y)
+                    elif string_comand.split(' ')[0] == 'SL':
+                        for x_times in range(int(string_comand.split(' ')[1])):
+                            x+=1
+                            y+=1
+                            x = verify_x(w_values, x)
+                            y =  verify_y(w_values, y)
+                            draw_directions(screen, w_values, array_saved, pad, x, y)
+                    elif string_comand.split(' ')[0] == 'SO':
+                        print(string_comand.split(' ')[1])
+                        for x_times in range(int(string_comand.split(' ')[1])):
+                            y-=1
+                            x+=1
+                            x = verify_x(w_values, x)
+                            y = verify_y(w_values, y)
+                            draw_directions(screen, w_values, array_saved, pad, x, y)
+                    
+                    elif string_comand.split(' ')[0] == 'O':
+                        print(string_comand.split(' ')[1])
+                        for x_times in range(int(string_comand.split(' ')[1])):
+                            y-=1
+                            y = verify_y(w_values, y)
+                            draw_directions(screen, w_values, array_saved, pad, x, y)
+                    
+                    elif string_comand.split(' ')[0] == 'L':
+                        print(string_comand.split(' ')[1])
+                        for x_times in range(int(string_comand.split(' ')[1])):
+                            y+=1
+                            y = verify_y(w_values, y)
+                            draw_directions(screen, w_values, array_saved, pad, x, y)
                             
-                            pcx = math.floor((w_values[0]-3)/2)+x
-                            pcy = math.floor(w_values[1]/2)+y
-                            print(array_saved)
-                            for x2 in array_saved:
-                                if x2[0] != pcx or x2[1] != pcy:
-                                    screen.addstr(x2[0], x2[1], '*')
-                                else:
-                                    screen.addstr(x2[0], x2[1], '@')
-                            
-                            if [pcx, pcy] not in array_saved:
-                                array_saved.append([pcx,pcy ])
-                            
-                            screen.refresh()
-                            pad.refresh(0,0,math.floor((w_values[0]-3)/2)+x,math.floor((w_values[1])/2)+y,math.floor((w_values[0] -3)/2)+x,math.floor(w_values[1]/2)+y)
-                            
-                            
-                            time.sleep(0.25)
+                    
                     
                 turtle_commands.clear()
                 pad_2.clear()
-                pad_2.addstr(1,1, "Comando turtle: ")
+                desenha_tela(pad_2, 1,1, "Comando turtle: ")
                 for x_turtle in range(len(turtle_commands)):
                     pad_2.addstr(1,18 + x_turtle, turtle_commands[x_turtle])
                 pad_2.box('|', '-')
-                pad_2.refresh(0,0,math.floor((w_values[0]-3)),1,math.floor((w_values[0]-1)),math.floor(w_values[1]-1))
+                refresh_pad2(pad_2, w_values)
 
             elif (ord(key)>=65 and ord(key)<=90) or (ord(key)>=48 and ord(key)<=57):
-                pad_2.addstr(1, 18 + len(turtle_commands), key)
+                desenha_tela(pad_2, 1,18+len(turtle_commands), key)
                 turtle_commands.append(key)
-                pad_2.refresh(0,0,math.floor((w_values[0]-3)),1,math.floor((w_values[0]-1)),math.floor(w_values[1]-1))
+                refresh_pad2(pad_2, w_values)
             elif ord(key) == 32:
-                pad_2.addstr(1, 18 + len(turtle_commands), key)
+                desenha_tela(pad_2, 1,18+len(turtle_commands), key)
                 turtle_commands.append(' ')
-                pad_2.refresh(0,0,math.floor((w_values[0]-3)),1,math.floor((w_values[0]-1)),math.floor(w_values[1]-1))
+                refresh_pad2(pad_2, w_values)
 
         except:
             pass
 
-        if key == 'KEY_A2':
+        if key == 'KEY_A2' or key == 'KEY_UP':
             x-=1
-        elif key == 'KEY_C2':
+            x = verify_x(w_values, x)
+        elif key == 'KEY_C2' or key == 'KEY_DOWN':
             x+=1
-        elif key == 'KEY_B3':
+            x = verify_x(w_values, x)
+        elif key == 'KEY_B3' or key == 'KEY_LEFT':
             y+=1
-        elif key == 'KEY_B1':
-            y-=1    
-        screen.clear()
-        screen.box('|','-')
-        screen.refresh()
+            y = verify_y(w_values,y)
+        elif key == 'KEY_B1' or key == 'KEY_RIGHT':
+            y-=1 
+            y = verify_y(w_values,y)
+        refresh_main_screen(screen)
         pcx = math.floor((w_values[0]-3)/2)+x
         pcy = math.floor(w_values[1]/2)+y
         print(array_saved)
         for x2 in array_saved:
             if x2[0] != pcx or x2[1] != pcy:
-                screen.addstr(x2[0], x2[1], '*')
+                desenha_tela(screen, x2[0], x2[1], '*')
             else:
-                screen.addstr(x2[0], x2[1], '@')
+                desenha_tela(screen, x2[0], x2[1], '@')
         
+        if [math.floor((w_values[0]-3)/2), math.floor(w_values[1]/2)] not in array_saved:
+            array_saved.append([math.floor((w_values[0]-3)/2), math.floor(w_values[1]/2)])
         if [pcx, pcy] not in array_saved:
             array_saved.append([pcx,pcy ])
         
-        
-        pad.refresh(0,0,math.floor((w_values[0]-3)/2)+x,math.floor((w_values[1])/2)+y,math.floor((w_values[0] -3)/2)+x,math.floor(w_values[1]/2)+y)
-        
-        
-
-        
-        
+        desenha_tela(screen, pcx,pcy, '@')
+        refresh_tela(pad, w_values, x, y)
 
 curses.wrapper(run)
